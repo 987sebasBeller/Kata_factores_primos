d092b772a29237bb0b189ab191f9e6e1c034c1dc
987sebasBeller <sebastianbeller76@gmail.com>
2023-09-24 15:22:06 -0400

    Refactoring de pruebas

--- src/factorePrimos.spec.js
+++ src/factorePrimos.spec.js
@@ -1,32 +1,36 @@
 import FactoresPrimos from "./factoresPrimos";
 describe("factores primos de un numero",()=>{
     it("devuelve los factores primos para el numero 1",()=>{
-        let factoresPrimos= new FactoresPrimos();
-        expect(factoresPrimos.generar(1)).toEqual([]);
+         
+        expect( generar(1)).toEqual([]);
     });
     it("devuelve los factores primos para el numero 2",()=>{
-        let factoresPrimos= new FactoresPrimos();
-        expect(factoresPrimos.generar(2)).toEqual([2]);
+         
+        expect( generar(2)).toEqual([2]);
     });
     it("devuelve los factores primos para el numero 3",()=>{
-        let factoresPrimos= new FactoresPrimos();
-        expect(factoresPrimos.generar(3)).toEqual([3]);
+         
+        expect( generar(3)).toEqual([3]);
     });
     it("devuelve los factores primos para el numero 4",()=>{
-        let factoresPrimos= new FactoresPrimos();
-        expect(factoresPrimos.generar(4)).toEqual([2,2]);
+         
+        expect( generar(4)).toEqual([2,2]);
     });
     it("devuelve los factores primos para el numero 6",()=>{
-        let factoresPrimos= new FactoresPrimos();
-        expect(factoresPrimos.generar(6)).toEqual([2,3]);
+         
+        expect( generar(6)).toEqual([2,3]);
     });
     it("devuelve los factores primos para el numero 8",()=>{
-        let factoresPrimos= new FactoresPrimos();
-        expect(factoresPrimos.generar(8)).toEqual([2,2,2]);
+         
+        expect( generar(8)).toEqual([2,2,2]);
     });
     
     it("devuelve los factores primos para el numero 9",()=>{
-        let factoresPrimos= new FactoresPrimos();
-        expect(factoresPrimos.generar(9)).toEqual([3,3]);
+         
+        expect( generar(9)).toEqual([3,3]);
     });
 });
+function generar(numero){
+     let factoresPrimos= new FactoresPrimos();
+    return factoresPrimos.generar(numero);
+}
